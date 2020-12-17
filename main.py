6a1c9af7df27c9f33dc0a7ac9968adf06fe14a87
Petr Jílek <petrjilek@centrum.cz>
2020-12-17 14:56:47 +0900

    Jupyter added

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ import neural_networks.nn3layers as nn
 import examples.basic_nn3layer as basicExample
 import examples.separators.lineSeparator as lSep
 import examples.separators.circleSeparator as cSep
+import models.dataGenerator as generator
+import models.separationFunctions as sf
 
 """Basic example of training neural network for desired output."""
-basicExample.run()
+# basicExample.run()
 
 """Example of linear separation."""
 # lSep.run(numTrainingData=10000, numValidationData=10000, learningRate=0.2)
